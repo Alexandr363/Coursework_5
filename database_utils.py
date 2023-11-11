af3e8461a1baaf2169c698438174aebb3491bc8b
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-11-11 22:32:40 +0300

    реализовал загрузку данных в таблицы

--- database_utils.py
+++ database_utils.py
@@ -1,14 +1,17 @@
 import psycopg2
+from vacancy_util import get_employees, vacancy
 
 
 def create_database(db_name):
-    conn = None
+    """Создаём базу данных и 2 таблицы"""
+
     conn = psycopg2.connect(dbname='postgres', host='localhost',
                             user='postgres', password='admin', port=5432)
     conn.autocommit = True
     cur = conn.cursor()
     cur.execute(f'DROP DATABASE IF EXISTS {db_name}')
     cur.execute(f'CREATE DATABASE {db_name}')
+    cur.close()
     conn.close()
 
     conn = psycopg2.connect(dbname=db_name, host='localhost', user='postgres',
@@ -32,5 +35,47 @@ def create_database(db_name):
                 url_employees TEXT
                 )
                 """)
+    cur.close()
+    conn.close()
+
+
+def db_load_data(db_name, user_employees):
+    """Загружаем данные в таблицы"""
+
+    employees = get_employees()
+    conn = psycopg2.connect(dbname=db_name, host='localhost', user='postgres',
+                            password='admin', port=5432)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    for employee in employees['items']:
+        cur.execute("""INSERT INTO  employees (
+                    id_company, company_name, vacancy_count, url_employees)
+                    VALUES (%s, %s, %s, %s)""",
+                    (employee['id'],
+                     employee['name'],
+                     employee['open_vacancies'],
+                     employee['alternate_url']))
+    cur.close()
+    conn.close()
+
+    conn = psycopg2.connect(dbname=db_name, host='localhost', user='postgres',
+                            password='admin', port=5432)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    # user_employees - список выбранных юзером ID работодателей
+    for id_employee in user_employees:
+        emp_vac = vacancy(id_employee)['items']
+        for emp in emp_vac:
+            cur.execute("""INSERT INTO  vacancies (
+                        id_vacancy, id_company, vacancy_name, salary, 
+                        url_vacancy)
+                        VALUES (%s, %s, %s, %s, %s)""",
+                        (emp['id'],
+                         emp['id'],
+                         emp['name'],
+                         emp['salary']['from'],
+                         emp['alternate_url']))
     cur.close()
     conn.close()
--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from vacancy_util import get_employees
-from database_utils import create_database
+from database_utils import create_database, db_load_data
 
-# vacancy = []
-# func_employee = get_employees()
-#
-# # Выводим список работодателей
-# for i in range(len(func_employee)):
-#     # записываем все ID работодателей для дальнейшей сверки с выбором юзера
-#     vacancy.append(func_employee['items'][i]['id'])
-#     print(f"Компания {func_employee['items'][i]['name']}  "
-#           f"id = {func_employee['items'][i]['id']}  "
-#           f"вакансий {func_employee['items'][i]['open_vacancies']}\n"
-#           f"ссылка на компанию  {func_employee['items'][i]['alternate_url']}")
-#     print()
-#
-# user_employees = []
-#
-# # Юзер выбирает ID работодателей для получения вакансий
-# while True:
-#     user = input('выберете id компании или введите 0 для выхода: \n')
-#     if user == '0':
-#         break
-#     if user in vacancy:
-#         user_employees.append(user)
-#     else:
-#         print('неверно введён id')
-#     print(f'количество выбранных компаний:  {len(user_employees)}')
-# print()
+vacancy = []
+func_employee = get_employees()
 
+# Выводим список работодателей
+for i in range(len(func_employee)):
+    # записываем все ID работодателей для дальнейшей сверки с выбором юзера
+    vacancy.append(func_employee['items'][i]['id'])
+    print(f"Компания {func_employee['items'][i]['name']}  "
+          f"id = {func_employee['items'][i]['id']}  "
+          f"вакансий {func_employee['items'][i]['open_vacancies']}\n"
+          f"ссылка на компанию  {func_employee['items'][i]['alternate_url']}")
+    print()
+
+user_employees = []
+
+# Юзер выбирает ID работодателей для получения вакансий
+while True:
+    user = input('выберете id компании или введите 0 для выхода: \n')
+    if user == '0':
+        break
+    if user in vacancy:
+        user_employees.append(user)
+    else:
+        print('неверно введён id')
+    print(f'количество выбранных компаний:  {len(user_employees)}')
+print()
+
+# Создаём базу данных
 db_name = input('Введите название базы данных: ')
 create_database(db_name)
 print(f"Создана база данных {db_name} и две таблицы: employees и  vacancies")
+
+# Загружаем данные в таблицы
+db_load_data(db_name, user_employees=user_employees)
+print('Данные загружены в таблицы')
